b652c95b6b5072a492dea9f4d94835581f6a99b7
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-12-09 13:47:40 +0100

    Add api class

---
+++ source/php/Api.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace VolunteerManager;
+
+/**
+ * Filtering WordPress API
+ */
+
+class Api
+{
+    private $removeFields;
+
+    public function __construct()
+    {
+        //Actions
+        add_action('template_redirect', array($this, 'redirectToApi'));
+
+        //Filters
+        add_filter('rest_url_prefix', array($this, 'apiBasePrefix'), 5000, 1);
+        add_filter('rest_prepare_alarm', array($this, 'removeResponseKeys'), 5000, 3);
+        add_filter('rest_prepare_station', array($this, 'removeResponseKeys'), 5000, 3);
+    }
+
+    /**
+     * Rename /wp-json/ to /json/.
+     * @return string Returning "json".
+     */
+    public function apiBasePrefix($prefix)
+    {
+        return "json";
+    }
+
+    /**
+     * Force the usage of wordpress api
+     * @return void
+     */
+    public static function redirectToApi()
+    {
+        if (php_sapi_name() === 'cli') {
+            return;
+        }
+
+        if (!is_admin() && strpos(self::currentUrl(), rtrim(rest_url(), "/")) === false && self::currentUrl() == rtrim(home_url(), "/")) {
+            wp_redirect(rest_url());
+            exit;
+        }
+    }
+
+    public function removeResponseKeys($response, $post, $request)
+    {
+        //Common keys
+        $keys = array('author', 'acf', 'guid', 'link', 'template', 'meta', 'taxonomy', 'menu_order');
+
+        //Do filtering
+        $response->data = array_filter($response->data, function ($k) use ($keys) {
+            return !in_array($k, $keys, true);
+        }, ARRAY_FILTER_USE_KEY);
+
+        //Return santizied response
+        return $response;
+    }
+
+    public static function currentUrl()
+    {
+        $currentURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
+        $currentURL .= $_SERVER["SERVER_NAME"];
+
+        if ($_SERVER["SERVER_PORT"] != "80" && $_SERVER["SERVER_PORT"] != "443") {
+            $currentURL .= ":".$_SERVER["SERVER_PORT"];
+        }
+
+        $currentURL .= $_SERVER["REQUEST_URI"];
+
+        return rtrim($currentURL, "/");
+    }
+}
