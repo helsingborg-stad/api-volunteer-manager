96189a93083409849f5b7b0a469f17c791a7a0a8
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-12-14 15:17:29 +0100

    Update buildscript

--- build.php
+++ build.php
@@ -7,11 +7,10 @@ if (php_sapi_name() !== 'cli') {
 
 // Any command needed to run and build plugin assets when newly cheched out of repo.
 $buildCommands = [
-    'npm ci --no-progress',
-    'npx browserslist@latest --update-db',
+    'npm ci --no-progress --no-audit',
+    'npx --yes browserslist@latest --update-db',
     'npm run build',
-    'composer install --prefer-dist --no-progress --no-dev',
-    'composer dump-autoload --no-dev --classmap-authoritative'
+    'composer install --prefer-dist --no-progress'
 ];
 
 // Files and directories not suitable for prod to be removed.
@@ -25,10 +24,7 @@ $removables = [
     'webpack.config.js',
     'node_modules',
     'package-lock.json',
-    'package.json',
-    'patchwork.json',
-    'phpunit.xml',
-    'source/tests'
+    'package.json'
 ];
 
 $dirName = basename(dirname(__FILE__));
@@ -38,8 +34,10 @@ $output = '';
 $exitCode = 0;
 foreach ($buildCommands as $buildCommand) {
     print "---- Running build command '$buildCommand' for $dirName. ----\n";
+    $timeStart = microtime(true);
     $exitCode = executeCommand($buildCommand);
-    print "---- Done build command '$buildCommand' for $dirName. ----\n";
+    $buildTime = round(microtime(true) - $timeStart);
+    print "---- Done build command '$buildCommand' for $dirName.  Build time: $buildTime seconds. ----\n";
     if ($exitCode > 0) {
         exit($exitCode);
     }
@@ -62,7 +60,14 @@ if (isset($argv[1]) && $argv[1] === '--cleanup') {
  */
 function executeCommand($command)
 {
-    $proc = popen("$command 2>&1 ; echo Exit status : $?", 'r');
+    $fullCommand = '';
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+        $fullCommand = "cmd /v:on /c \"$command 2>&1 & echo Exit status : !ErrorLevel!\"";
+    } else {
+        $fullCommand = "$command 2>&1 ; echo Exit status : $?";
+    }
+
+    $proc = popen($fullCommand, 'r');
 
     $liveOutput     = '';
     $completeOutput = '';
