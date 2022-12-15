6a4b923f977145f6f8b672d71ab38e32a658b118
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-12-15 14:41:59 +0100

    Implement contrasting label text

--- source/php/Helper/Admin/UI.php
+++ source/php/Helper/Admin/UI.php
@@ -18,8 +18,9 @@ class UI
         } else {
             foreach ((array)$taxonomy as $item) {
                 echo sprintf(
-                    '<span style="background: %s;" class="todo-term-pill %s">%s</span>',
+                    '<span style="background: %s; padding: 3px 7px; border-radius: 3px; color: %s;" class="term-pill term-pill-%s">%s</span>',
                     self::taxonomyColor($item->term_id, $item->taxonomy),
+                    self::taxonomyColorContrast($item->term_id, $item->taxonomy),
                     $item->slug,
                     $item->name
                 ); 
@@ -29,14 +30,34 @@ class UI
 
     /**
      * Returns colorcode by taxonomy id
+     *
+     * @param int $termId
+     * @param string $taxonomySlug
      * @return string
      */
-
     public static function taxonomyColor($termId, $taxonomySlug) : string
     {
         return Field::get(
             'taxonomy_color', 
             $taxonomySlug . '_' . $termId
         ) ?? '#eee'; 
+    }
+
+    /**
+     * Get contrast
+     *
+     * @param int $termId
+     * @param string $taxonomySlug
+     * @return string
+     */
+    public static function taxonomyColorContrast($termId, $taxonomySlug) {
+        $color = self::taxonomyColor($termId, $taxonomySlug); 
+            
+        $r = hexdec(substr($hexcolor, 1, 2));
+        $g = hexdec(substr($hexcolor, 3, 2));
+        $b = hexdec(substr($hexcolor, 5, 2));
+        $yiq = (($r * 299) + ($g * 587) + ($b * 114)) / 1000;
+        
+        return ($yiq >= 128) ? '#000' : '#fff';            
     }
 }
