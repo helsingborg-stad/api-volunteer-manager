1f8dc0eb99d241e63c00658872a14ae61aeaf9ab
zilaei <zzilaei@gmail.com>
2023-02-21 16:29:15 +0100

    create employee taxonomy and post table

--- source/php/Employee.php
+++ source/php/Employee.php
@@ -40,10 +40,50 @@ class Employee extends PostType
                 'exclude_from_search'   =>  true,
                 'taxonomies'            =>  array(),
                 'supports'              =>  array('title'),
-                'show_in_rest'          => true
+                'show_in_rest'          =>  true
+            )
+        );
+    }
+
+    /**
+     * Add table column to post type
+     *
+     * @param PostType $postType
+     * @return void
+     */
+    public function addPostTypeTableColumn(PostType $postType)
+    {
+        $postType->addTableColumn(
+            'registration_status',
+            __('Registration status', 'api-volunteer-manager'),
+            true,
+            function ($column, $postId) {
+                echo "-";
+            }
+        );
+    }
+
+    private function registerEmployeeStatusTaxonomy($postType) : string
+    {
+        $categories = new Taxonomy(
+            'Registration statuses',
+            'Registration status',
+            'employee-registration-status',
+            array($postType->slug),
+            array (
+                'hierarchical' => false,
+                // 'show_ui' => false
             )
         );
 
-        return $postType->slug;
+        return $categories->slug;
+    }
+
+    public function createEmployeeStatusTerms()
+    {
+        $insert_result = wp_insert_term(
+            'p_insert_test',
+            'employee-registration-status'
+        );
     }
 }
