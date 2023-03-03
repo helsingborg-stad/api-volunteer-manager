d786e9c0b0ff36a1988eae94f5ea5238f32ce809
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-03-03 15:07:21 +0100

    created notifications config

---
+++ source/php/Notification/Notifications.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'assignment' => [
+        'assignment-status' => [
+            [
+                'key' => 'assignment_approved',
+                'oldValue' => 'pending',
+                'newValue' => 'approved',
+                'message' => [
+                    'subject' => 'Assignment approved subject',
+                    'content' => 'Assignment approved message',
+                ],
+                'rule' => [
+                    'key' => 'source',
+                    'value' => 'internal',
+                    'operator' => 'NOT_EQUAL'
+                ]
+            ],
+            [
+                'key' => 'assignment_denied',
+                'oldValue' => 'pending',
+                'newValue' => 'denied',
+                'message' => [
+                    'subject' => 'Assignment denied subject',
+                    'content' => 'Assignment denied message',
+                ],
+                'rule' => [
+                    'key' => 'source',
+                    'value' => 'internal',
+                    'operator' => 'NOT_EQUAL'
+                ]
+            ]
+        ]
+    ],
+    'volunteer' => [
+        // add notifications
+    ]
+];
