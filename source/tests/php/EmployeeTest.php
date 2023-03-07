31de5ce198d8d582ad62df60ab5fedf343ba0c7e
zilaei <zzilaei@gmail.com>
2023-03-07 13:22:47 +0100

    added test for Employee hooks

---
+++ source/tests/php/EmployeeTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace php;
+
+use Brain\Monkey\Expectation\Exception\ExpectationArgsRequired;
+use Brain\Monkey\Functions;
+use PluginTestCase\PluginTestCase;
+use VolunteerManager\Employee as Employee;
+
+class EmployeeTest extends PluginTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Functions\when('_x')->returnArg();
+    }
+
+    /**
+     * @throws ExpectationArgsRequired
+     */
+    public function testAddHooks()
+    {
+        $employee = new Employee();
+
+        Functions\expect('add_action')
+            ->once()
+            ->with('init', [$employee, 'createEmployeeStatusTerms']);
+
+        $employee->addHooks();
+    }
+}
