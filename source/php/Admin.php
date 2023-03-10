68ef1904707e0c6db468015e1369c480f1a1b924
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-03-10 14:59:53 +0100

    added options page and refactored admin class

--- source/php/Admin.php
+++ source/php/Admin.php
@@ -4,21 +4,42 @@ namespace VolunteerManager;
 
 class Admin
 {
+    public function addHooks()
+    {
+        add_filter('get_sample_permalink_html', array($this, 'replacePermalink'), 10, 5);
+        add_action('acf/init', array($this, 'addOptionsPage'));
+    }
 
-  public function __construct() {
-    add_filter('get_sample_permalink_html', array($this, 'replacePermalink'), 10, 5);
-  }
+    /**
+     * Replaces permalink on edit post with API-url
+     * @param $return
+     * @param $post_id
+     * @param $new_title
+     * @param $new_slug
+     * @param $post
+     * @return string
+     */
+    public function replacePermalink($return, $post_id, $new_title, $new_slug, $post): string
+    {
+        $postType = $post->post_type;
+        $jsonUrl = home_url() . '/json/wp/v2/' . $postType . '/';
+        $apiUrl = $jsonUrl . $post_id;
 
-  /**
-   * Replaces permalink on edit post with API-url
-   * @return string
-   */
-  public function replacePermalink($return, $post_id, $new_title, $new_slug, $post)
-  {
-      $postType = $post->post_type;
-      $jsonUrl = home_url() . '/json/wp/v2/' . $postType . '/';
-      $apiUrl = $jsonUrl . $post_id;
+        return '<strong>' . __('API-url', 'api-volunteer-manager') . ':</strong> <a href="' . $apiUrl . '" target="_blank">' . $apiUrl . '</a>';
+    }
 
-      return '<strong>' . __('API-url', 'api-volunteer-manager') . ':</strong> <a href="' . $apiUrl . '" target="_blank">' . $apiUrl . '</a>';
-  }
+    /**
+     * Registers option page
+     * @return void
+     */
+    public function addOptionsPage(): void
+    {
+        acf_add_options_sub_page(array(
+            'page_title' => __('Volunteer options', 'api-volunteer-manager'),
+            'menu_title' => __('Options', 'api-volunteer-manager'),
+            'menu_slug' => 'options',
+            'parent_slug' => 'edit.php?post_type=assignment',
+            'capability' => 'install_themes'
+        ));
+    }
 }
