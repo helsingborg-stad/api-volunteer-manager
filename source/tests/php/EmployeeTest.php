e74e95215d2f0645f13e694db2efc37cbbc98233
zilaei <zzilaei@gmail.com>
2023-03-16 16:28:01 +0100

    test for injecting Term to Employee

--- source/tests/php/EmployeeTest.php
+++ source/tests/php/EmployeeTest.php
@@ -6,6 +6,7 @@ use Brain\Monkey\Expectation\Exception\ExpectationArgsRequired;
 use Brain\Monkey\Functions;
 use PluginTestCase\PluginTestCase;
 use VolunteerManager\Employee as Employee;
+use VolunteerManager\Entity\Term;
 
 class EmployeeTest extends PluginTestCase
 {
@@ -28,6 +29,10 @@ class EmployeeTest extends PluginTestCase
      */
     public function testAddHooks()
     {
+        $termMock = \Mockery::mock(Term::class);
+
+        $employee = new Employee($termMock);
+
         Functions\expect('add_action')
             ->once()
             ->with('init', [$this->employee, 'insertEmploymentStatusTerms']);
@@ -104,5 +109,40 @@ class EmployeeTest extends PluginTestCase
                 ['to' => '', 'from' => '', 'message' => ['subject' => 'Subject', 'content' => 'Content']]
             ],
         ];
+    }
+
+    public function testInsertEmployeeStatusTerms()
+    {
+        $termMock = \Mockery::mock(Term::class);
+        $term_items = [
+            [
+                'name' => 'New',
+                'slug' => 'new',
+                'description' => 'New employee. Employee needs to be processed.'
+            ],
+            [
+                'name' => 'Ongoing',
+                'slug' => 'ongoing',
+                'description' => 'Employee under investigation.'
+            ],
+            [
+                'name' => 'Approved',
+                'slug' => 'approved',
+                'description' => 'Employee approved for assignments.'
+            ],
+            [
+                'name' => 'Denied',
+                'slug' => 'denied',
+                'description' => 'Employee denied. Employee can\'t apply.'
+            ]
+        ];
+
+        $termMock->shouldReceive('insertTerms')
+            ->once()
+            ->with($term_items, 'employee-registration-status')
+            ->andReturn(['term_id' => 1, 'term_taxonomy_id' => 1]);
+
+        $employee = new Employee($termMock);
+        $employee->insertEmploymentStatusTerms();
     }
 }
