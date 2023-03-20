bbf9e9a80dadd6cc5764eba95b0f9ff7e5faba9f
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-03-20 09:29:17 +0100

    flattened config file and added taxonomy field

--- source/php/Notification/NotificationsConfig.php
+++ source/php/Notification/NotificationsConfig.php
@@ -5,40 +5,50 @@ namespace VolunteerManager\Notification;
 class NotificationsConfig
 {
     public static array $notifications = [
-        'assignment' => [
-            'assignment-status' => [
-                [
-                    'key' => 'assignment_approved',
-                    'oldValue' => 'pending',
-                    'newValue' => 'approved',
-                    'message' => [
-                        'subject' => 'Assignment approved subject',
-                        'content' => 'Assignment approved message',
-                    ],
-                    'rule' => [
-                        'key' => 'source',
-                        'value' => '',
-                        'operator' => 'NOT_EQUAL'
-                    ]
-                ],
-                [
-                    'key' => 'assignment_denied',
-                    'oldValue' => 'pending',
-                    'newValue' => 'denied',
-                    'message' => [
-                        'subject' => 'Assignment denied subject',
-                        'content' => 'Assignment denied message',
-                    ],
-                    'rule' => [
-                        'key' => 'source',
-                        'value' => '',
-                        'operator' => 'NOT_EQUAL'
-                    ]
-                ]
+        [
+            'key' => 'external_assignment_approved',
+            'taxonomy' => 'assignment-status',
+            'oldValue' => 'pending',
+            'newValue' => 'approved',
+            'message' => [
+                'subject' => 'Assignment approved subject',
+                'content' => 'Assignment approved message',
+            ],
+            'rule' => [
+                'key' => 'source',
+                'value' => '',
+                'operator' => 'NOT_EQUAL'
             ]
         ],
-        'volunteer' => [
-            // add notifications
+        [
+            'key' => 'external_assignment_denied',
+            'taxonomy' => 'assignment-status',
+            'oldValue' => 'pending',
+            'newValue' => 'denied',
+            'message' => [
+                'subject' => 'Assignment denied subject',
+                'content' => 'Assignment denied message',
+            ],
+            'rule' => [
+                'key' => 'source',
+                'value' => '',
+                'operator' => 'NOT_EQUAL'
+            ]
+        ],
+        [
+            'key' => 'external_volunteer_new',
+            'taxonomy' => 'employee-registration-status',
+            'oldValue' => '',
+            'newValue' => 'new',
+            'message' => [
+                'subject' => 'Application received',
+                'content' => 'Your application has been received. More info here...',
+            ],
+            'rule' => [
+                'key' => 'source',
+                'value' => '',
+                'operator' => 'NOT_EQUAL'
+            ]
         ]
     ];
 }
