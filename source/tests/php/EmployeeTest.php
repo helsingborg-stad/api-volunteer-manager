3540c0a7cd0bd998109d6639008ead21dc71b2ea
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-03-20 10:49:43 +0100

    added tests

--- source/tests/php/EmployeeTest.php
+++ source/tests/php/EmployeeTest.php
@@ -9,6 +9,20 @@ use VolunteerManager\Employee as Employee;
 
 class EmployeeTest extends PluginTestCase
 {
+
+    private $employee;
+    private object $post;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->post = new \stdClass();
+        $this->post->ID = 99;
+
+        $this->employee = new Employee();
+    }
+
     /**
      * @throws ExpectationArgsRequired
      */
@@ -21,5 +35,61 @@ class EmployeeTest extends PluginTestCase
             ->with('init', [$employee, 'insertEmploymentStatusTerms']);
 
         $employee->addHooks();
+    }
+
+    /**
+     * @dataProvider populateNotificationReceiverProvider
+     */
+    public function testPopulateNotificationWithSubmitter($args, $expectedResult, $getFieldResult)
+    {
+        Functions\when('get_field')->justReturn($getFieldResult);
+        $this->assertEquals(
+            $expectedResult,
+            $this->employee->populateNotificationWithReceiver($args, $this->post->ID)
+        );
+    }
+
+    public function populateNotificationReceiverProvider(): array
+    {
+        return [
+            "Existing email address" => [
+                [
+                    'to' => '',
+                    'from' => 'from@email.com',
+                    'message' => [
+                        'subject' => 'subject',
+                        'content' => 'content',
+                    ]
+                ],
+                [
+                    'to' => 'foo@bar.com',
+                    'from' => 'from@email.com',
+                    'message' => [
+                        'subject' => 'subject',
+                        'content' => 'content',
+                    ]
+                ],
+                'foo@bar.com'
+            ],
+            "Missing email address" => [
+                [
+                    'to' => '',
+                    'from' => 'from@email.com',
+                    'message' => [
+                        'subject' => 'subject',
+                        'content' => 'content',
+                    ]
+                ],
+                [
+                    'to' => '',
+                    'from' => 'from@email.com',
+                    'message' => [
+                        'subject' => 'subject',
+                        'content' => 'content',
+                    ]
+                ],
+                null
+            ],
+        ];
     }
 }
