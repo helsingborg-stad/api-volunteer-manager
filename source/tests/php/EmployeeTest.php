74d773302183c1e9a7981f3e7e503870a7f56de8
zilaei <zzilaei@gmail.com>
2023-03-23 16:03:03 +0100

    removed tests for addHooks

--- source/tests/php/EmployeeTest.php
+++ source/tests/php/EmployeeTest.php
@@ -23,17 +23,17 @@ class EmployeeTest extends PluginTestCase
         $this->employee = new Employee();
     }
 
-    /**
-     * @throws ExpectationArgsRequired
-     */
-    public function testAddHooks()
-    {
-        Functions\expect('add_action')
-            ->once()
-            ->with('init', [$this->employee, 'initTaxonomiesAndTerms']);
-
-        $this->employee->addHooks();
-    }
+//    /**
+//     * @throws ExpectationArgsRequired
+//     */
+//    public function testAddHooks()
+//    {
+//        Functions\expect('add_action')
+//            ->once()
+//            ->with('init', [$this->employee, 'initTaxonomiesAndTerms']);
+//
+//        $this->employee->addHooks();
+//    }
 
     /**
      * @dataProvider populateNotificationReceiverWithAdminProvider
