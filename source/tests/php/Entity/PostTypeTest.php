303683284eabe92253cfca6a40cbda27f77f65a7
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-03-27 10:07:22 +0200

    removed old test for tableColumnsContent

--- source/tests/php/Entity/PostTypeTest.php
+++ source/tests/php/Entity/PostTypeTest.php
@@ -120,33 +120,8 @@ class PostTypeTest extends PluginTestCase
 
     /**
      * @dataProvider postTypeProvider
-     * @throws \Brain\Monkey\Expectation\Exception\ExpectationArgsRequired
      */
     public function testTableColumnsContent($args)
-    {
-        $customPostType = new PostTypeNew(...$args);
-
-        $mockCallback = function (string $column, int $postId) {
-            $this->assertEquals('column1', $column);
-            $this->assertEquals(123, $postId);
-            echo $postId;
-        };
-
-        $customPostType->tableColumnsContentCallback = ['column1' => $mockCallback];
-
-        Functions\expect('call_user_func_array')
-            ->once()
-            ->with($mockCallback, ['column1', 123]);
-
-        $customPostType->tableColumnsContent('column1', 123);
-    }
-
-    /**
-     * Alternative test for tableColumnsContent method
-     *
-     * @dataProvider postTypeProvider
-     */
-    public function testTableColumnsContentV2($args)
     {
         $customPostType = new PostTypeNew(...$args);
 
@@ -166,5 +141,4 @@ class PostTypeTest extends PluginTestCase
         $expectedOutput = "Column: {$columnKey}, post ID: {$postId}";
         $this->assertEquals($expectedOutput, $output);
     }
-
 }
