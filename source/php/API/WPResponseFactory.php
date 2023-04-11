f14845eaf39b9d5241e7287083c618a0012b119a
zilaei <zzilaei@gmail.com>
2023-04-11 16:15:09 +0200

    added WPResponseFactory
    Handler for API responses.

--- source/php/API/WPResponseFactory.php
+++ source/php/API/WPResponseFactory.php
@@ -3,6 +3,7 @@
 namespace VolunteerManager\API;
 
 use WP_Error;
+use WP_REST_Response;
 
 class WPResponseFactory
 {
@@ -23,6 +24,30 @@ class WPResponseFactory
                 'status' => 400,
                 'param' => $param
             )
+        );
+    }
+
+    /**
+     * Create a WP_REST_Response
+     *
+     * @param string $message Success message.
+     * @param int $employee_id Employee ID.
+     * @param int $status Status code.
+     * @return WP_REST_Response
+     */
+    public static function wp_rest_response(
+        string $message,
+        int    $employee_id,
+        int    $status = 200
+    ): WP_REST_Response
+    {
+        return new WP_REST_Response(
+            array(
+                'status' => $status,
+                'message' => $message,
+                'employee_id' => $employee_id
+            ),
+            $status
         );
     }
 }
