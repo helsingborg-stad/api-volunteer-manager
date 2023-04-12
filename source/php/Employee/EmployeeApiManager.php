71f93e85d52dda96f46f4b4f38b893b7acd73c64
zilaei <zzilaei@gmail.com>
2023-04-12 09:01:13 +0200

    removed todo

--- source/php/Employee/EmployeeApiManager.php
+++ source/php/Employee/EmployeeApiManager.php
@@ -10,7 +10,6 @@ use WP_REST_Response;
 
 class EmployeeApiManager
 {
-    // TODO: Set correct endpoint. This is just a placeholder.
     public function registerPostEndpoint()
     {
         (new Api())->registerPostEndpoint(
