b4b93ecae60857004a16fd6a73c1eac85023cb30
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-04-24 15:57:16 +0200

    fixed new notice texts

--- source/php/PostType/Application/ApplicationNotificationFilters.php
+++ source/php/PostType/Application/ApplicationNotificationFilters.php
@@ -10,6 +10,9 @@ class ApplicationNotificationFilters extends NotificationFilters
     {
         add_filter('avm_admin_external_application_new_notification', array($this, 'populateNotificationReceiverWithAdmin'), 10, 2);
         add_filter('avm_admin_external_application_new_notification', array($this, 'populateAdminNotificationWithContent'), 10, 2);
+
+        add_filter('avm_external_application_new_notification', array($this, 'populateReceiverWithSubmitter'), 10, 2);
+        add_filter('avm_external_application_new_notification', array($this, 'populateApplicationWithContent'), 10, 2);
     }
 
     /**
@@ -26,6 +29,39 @@ class ApplicationNotificationFilters extends NotificationFilters
             $args['content'],
             $assignment->post_title,
             $adminUrl
+        );
+        return $args;
+    }
+
+    /**
+     * Populate notification receiver with submitter email address
+     * @param array $args
+     * @param int   $postId
+     * @return array
+     */
+    public function populateReceiverWithSubmitter(array $args, int $postId): array
+    {
+        $employee = get_field('application_employee', $postId);
+        $receiver = get_field('email', $employee->ID);
+        $args['to'] = $receiver ?? '';
+        return $args;
+    }
+
+    /**
+     * Populate notification with subject and content
+     * @param array $args
+     * @param int   $postId
+     * @return array
+     */
+    public function populateApplicationWithContent(array $args, int $postId): array
+    {
+        $employee = get_field('application_employee', $postId);
+        $name = get_field('first_name', $employee->ID);
+        $assignment = get_field('application_assignment', $postId);
+        $args['content'] = sprintf(
+            $args['content'],
+            $name,
+            $assignment->post_title
         );
         return $args;
     }
