131f8a3fe490f025e569f234a004109d411ff0a8
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-04-27 13:05:27 +0200

    display category taxonomy

--- source/php/AcfFields/php/assignment-status.php
+++ source/php/AcfFields/php/assignment-status.php
@@ -3,7 +3,7 @@
 if (function_exists('acf_add_local_field_group')) {
     acf_add_local_field_group(array(
     'key' => 'group_63e2023f5baca',
-    'title' => __('Status', 'api-volunteer-manager'),
+    'title' => __('Taxonomier', 'api-volunteer-manager'),
     'fields' => array(
         0 => array(
             'key' => 'field_63e2023fc3a36',
@@ -46,6 +46,32 @@
                 'id' => '',
             ),
             'taxonomy' => 'assignment-eligibility',
+            'add_term' => 0,
+            'save_terms' => 1,
+            'load_terms' => 1,
+            'return_format' => 'object',
+            'field_type' => 'select',
+            'allow_null' => 0,
+            'acfe_bidirectional' => array(
+                'acfe_bidirectional_enabled' => '0',
+            ),
+            'multiple' => 0,
+        ),
+        2 => array(
+            'key' => 'field_644a38a755b10',
+            'label' => __('Category', 'api-volunteer-manager'),
+            'name' => 'assignment_category',
+            'aria-label' => '',
+            'type' => 'taxonomy',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'taxonomy' => 'assignment-category',
             'add_term' => 0,
             'save_terms' => 1,
             'load_terms' => 1,
