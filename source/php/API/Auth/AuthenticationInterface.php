a5a5f1c1905eafd08a57771e09b5d28a6fcaf754
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-05-16 14:13:05 +0200

    return type fix

--- source/php/API/Auth/AuthenticationInterface.php
+++ source/php/API/Auth/AuthenticationInterface.php
@@ -9,7 +9,7 @@ interface AuthenticationInterface
     /**
      * Validate REST request with JWT
      * @param WP_REST_Request $request
-     * @return mixed Returns WP_REST_Request on success, or WP_Error on failure.
+     * @return \WP_Error|\WP_REST_Request
      */
-    public function validateRequest(WP_REST_Request $request): WP_REST_Request;
+    public function validateRequest(WP_REST_Request $request);
 }
