c41b4551ee0472bd6b1288a3a6e25c704033ca79
zilaei <zzilaei@gmail.com>
2023-05-16 12:19:24 +0200

    api: concrete implementation of FormatRequest

---
+++ source/php/API/FormatRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace VolunteerManager\API;
+
+use WP_Error;
+use WP_REST_Request;
+
+class FormatRequest implements RestFormatInterface
+{
+    
+    /**
+     * @inheritDoc
+     */
+    public function formatRestRequest(WP_REST_Request $request): WP_REST_Request|WP_Error
+    {
+        return $request;
+    }
+}
