bcdebd45a1afd59e9e8c65cdc0e9b91512058707
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-05-23 10:11:50 +0200

    updated interface return

--- source/php/API/Auth/AuthenticationInterface.php
+++ source/php/API/Auth/AuthenticationInterface.php
@@ -9,7 +9,7 @@ interface AuthenticationInterface
     /**
      * Validate REST request with JWT
      * @param WP_REST_Request $request
-     * @return \WP_Error|\WP_REST_Request
+     * @return \WP_Error|array
      */
     public function validateRequest(WP_REST_Request $request);
 }
