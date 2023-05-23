31203d69734d923d0742f2c95961b8f0e6b2f910
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-05-23 10:10:42 +0200

    populate request params with token content

--- source/php/API/Auth/AuthenticationDecorator.php
+++ source/php/API/Auth/AuthenticationDecorator.php
@@ -2,6 +2,8 @@
 
 namespace VolunteerManager\API\Auth;
 
+use WP_REST_Request;
+
 class AuthenticationDecorator
 {
     private $callback;
@@ -13,7 +15,7 @@ class AuthenticationDecorator
         $this->authentication = $authentication;
     }
 
-    public function __invoke($request)
+    public function __invoke(WP_REST_Request $request)
     {
         $validatedRequest = $this->authentication->validateRequest($request);
 
@@ -21,6 +23,20 @@ class AuthenticationDecorator
             return $validatedRequest;
         }
 
-        return call_user_func($this->callback, $validatedRequest);
+        $newRequest = $this->setTokenRequestParams($request, $validatedRequest);
+
+        return call_user_func($this->callback, $newRequest);
+    }
+
+    public function setTokenRequestParams(WP_REST_Request $request, array $params): WP_REST_Request
+    {
+        $request->set_param('session_id', $params['sessionId'] ?? null);
+        $request->set_param('national_identity_number', $params['id'] ?? null);
+        $request->set_param('name', $params['name'] ?? null);
+        $request->set_param('first_name', $params['firstName'] ?? null);
+        $request->set_param('surname', $params['lastName'] ?? null);
+        $request->set_param('source', isset($params['iss']) ? stripslashes($params['iss']) : null);
+        $request->set_param('aud', isset($params['aud']) ? stripslashes($params['aud']) : null);
+        return $request;
     }
 }
