00e3d0c570f82c1022168a14ebb8bd89dd419d98
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-05-26 08:13:12 +0200

    Feat/get employee (#47)
    * disable all public rest requests
    
    * inject params instead to make class more flexible
    
    * use update_field to save meta with ACF
    
    * send required params as args
    
    * added GET employee request
    
    * refactored formatting into own method
    
    * added status to employee response
    
    * typo fix
    
    * changed class order
    
    * save date and newsletter meta data

--- source/php/PostType/Employee/EmployeeApiManager.php
+++ source/php/PostType/Employee/EmployeeApiManager.php
@@ -6,6 +6,7 @@ use VolunteerManager\API\Auth\AuthenticationInterface;
 use VolunteerManager\API\Auth\AuthenticationDecorator;
 use VolunteerManager\API\FormatRequest;
 use VolunteerManager\API\WPResponseFactory;
+use WP_Error;
 use WP_REST_Request;
 use WP_REST_Response;
 
@@ -31,9 +32,17 @@ class EmployeeApiManager
             array(
                 'methods' => 'POST',
                 'callback' => new AuthenticationDecorator([$this, 'handlePostRequest'], $this->authentication),
-                'permission_callback' => function () {
-                    return true;
-                },
+                'permission_callback' => '__return_true'
+            )
+        );
+
+        register_rest_route(
+            'wp/v2',
+            'employee',
+            array(
+                'methods' => 'GET',
+                'callback' => new AuthenticationDecorator([$this, 'handleGetRequest'], $this->authentication),
+                'permission_callback' => '__return_true',
             )
         );
     }
@@ -42,7 +51,10 @@ class EmployeeApiManager
     {
         $format_request = new FormatRequest();
         $unique_params = new ValidateUniqueParams($format_request);
-        $required_params = new RequiredEmployeeParams($unique_params);
+        $required_params = new RequiredEmployeeParams(
+            $unique_params,
+            ['email', 'first_name', 'surname', 'national_identity_number']
+        );
 
         $validated_params = $required_params->formatRestRequest($request);
         if (is_wp_error($validated_params)) {
@@ -66,7 +78,8 @@ class EmployeeApiManager
             'surname',
             'national_identity_number',
             'phone_number',
-            'source'
+            'source',
+            'newsletter'
         ];
 
         $params = [];
@@ -86,8 +99,10 @@ class EmployeeApiManager
         );
 
         foreach ($params as $key => $value) {
-            update_post_meta($employeePostId, $key, $value);
+            update_field($key, $value, $employeePostId);
         }
+
+        update_field('registration_date', date('Y-m-d'), $employeePostId);
 
         $new_status_term = get_term_by('slug', 'new', 'employee-registration-status');
         if ($new_status_term) {
@@ -98,5 +113,87 @@ class EmployeeApiManager
             __('Employee created', AVM_TEXT_DOMAIN),
             $employeePostId
         );
+    }
+
+    public function handleGetRequest(WP_REST_Request $request)
+    {
+        $format_request = new FormatRequest();
+        $required_params = new RequiredEmployeeParams(
+            $format_request,
+            ['national_identity_number']
+        );
+        $validated_params = $required_params->formatRestRequest($request);
+        if (is_wp_error($validated_params)) {
+            return $validated_params;
+        }
+
+        $nationalIdentityNumber = $request->get_param('national_identity_number');
+        $employee = $this->getEmployeeByIdentityNumber($nationalIdentityNumber);
+
+        if (!$employee) {
+            return new WP_Error(
+                'rest_post_invalid_id',
+                'Invalid post ID.',
+                array(
+                    'status' => 404,
+                )
+            );
+        }
+
+        return new WP_REST_Response(
+            $this->getEmployeeDetails($employee->ID),
+            200
+        );
+    }
+
+    /**
+     * Retrieve employee data and format it into an array
+     *
+     * @param int $employeeId The ID of the employee
+     * @return array The formatted employee data
+     */
+    public function getEmployeeDetails(int $employeeId): array
+    {
+        $employeeFields = get_fields($employeeId);
+        $status = get_the_terms($employeeId, 'employee-registration-status');
+        $status = !empty($status[0]) ? [
+            'term_id' => $status[0]->term_id,
+            'name' => $status[0]->name,
+            'slug' => $status[0]->slug,
+        ] : null;
+
+        return [
+            'id' => $employeeId,
+            'national_identity_number' => $employeeFields['national_identity_number'] ?? null,
+            'first_name' => $employeeFields['first_name'] ?? null,
+            'surname' => $employeeFields['surname'] ?? null,
+            'email' => $employeeFields['email'] ?? null,
+            'phone_number' => $employeeFields['phone_number'] ?? null,
+            'newsletter' => $employeeFields['newsletter'] ?? null,
+            'registration_date' => $employeeFields['registration_date'] ?? null,
+            'status' => $status,
+        ];
+    }
+
+    /**
+     * Retrieve employee by national identity number
+     *
+     * @param string $nationalIdentityNumber The national identity number of the employee
+     * @return null|\WP_Post The employee data matching the national identity number
+     */
+    public function getEmployeeByIdentityNumber(string $nationalIdentityNumber): ?\WP_Post
+    {
+        $employee = get_posts(array(
+            'post_type' => 'employee',
+            'post_status' => 'any',
+            'meta_query' => array(
+                array(
+                    'key' => 'national_identity_number',
+                    'value' => $nationalIdentityNumber,
+                    'compare' => '=',
+                )
+            )
+        ));
+        return !empty($employee[0]) ? $employee[0] : null;
     }
 }
