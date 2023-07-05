ae7f952ed95a74f1837f42464971a7d263b8afd6
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-07-05 08:11:07 +0200

    add about employer field

--- source/php/AcfFields/php/employer.php
+++ source/php/AcfFields/php/employer.php
@@ -1,9 +1,7 @@
 <?php 
 
-
-if (function_exists('acf_add_local_field_group')) {
-
-    acf_add_local_field_group(array(
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
     'key' => 'group_639308fb101ce',
     'title' => __('Employer', 'api-volunteer-manager'),
     'fields' => array(
@@ -117,6 +115,27 @@ if (function_exists('acf_add_local_field_group')) {
             ),
         ),
         2 => array(
+            'key' => 'field_64a2749201991',
+            'label' => __('About', 'api-volunteer-manager'),
+            'name' => 'employer_about',
+            'aria-label' => '',
+            'type' => 'textarea',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'acfe_textarea_code' => 0,
+            'maxlength' => '',
+            'rows' => 4,
+            'placeholder' => '',
+            'new_lines' => '',
+        ),
+        3 => array(
             'key' => 'field_639af303f57b8',
             'label' => __('Website', 'api-volunteer-manager'),
             'name' => 'employer_website',
@@ -158,5 +177,4 @@ if (function_exists('acf_add_local_field_group')) {
     'acfe_meta' => '',
     'acfe_note' => '',
 ));
-
-}
+}
